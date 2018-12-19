b95b3e81b183a39514d619ed2f995251f0174686
Stefano <stefano.dafarra@gmail.com>
2018-12-19 12:04:15 +0100

    Implemented base class for QpIK.

---
+++ modules/Walking_module/src/WalkingQPInverseKinematics.cpp
@@ -0,0 +1,257 @@
+/**
+ * @file WalkingQPInverseKinematics.hpp
+ * @authors Giulio Romualdi <giulio.romualdi@iit.it>, Stefano Dafarra <stefano.dafarra@iit.it>
+ * @copyright 2018 iCub Facility - Istituto Italiano di Tecnologia
+ *            Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * @date 2018
+ */
+
+// std
+#include <cmath>
+
+// YARP
+#include <yarp/os/LogStream.h>
+#include <yarp/os/Value.h>
+
+// iDynTree
+#include <iDynTree/Core/EigenHelpers.h>
+#include <iDynTree/Core/EigenSparseHelpers.h>
+#include <iDynTree/Model/Model.h>
+#include "iDynTree/yarp/YARPConfigurationsLoader.h"
+
+#include <WalkingQPInverseKinematics.hpp>
+#include <Utils.hpp>
+
+bool WalkingQPIK::initializeMatrices(const yarp::os::Searchable& config)
+{
+    yarp::os::Value tempValue;
+
+    // evaluate constant sub-matrix of the hessian matrix
+    // get the CoM weight
+    if(!m_useCoMAsConstraint)
+    {
+        tempValue = config.find("comWeightTriplets");
+        iDynTree::Triplets comWeightMatrix;
+        if(!iDynTreeHelper::Triplets::getTripletsFromValues(tempValue, 3, comWeightMatrix))
+        {
+            yError() << "Initialization failed while reading comWeightTriplets vector.";
+            return false;
+        }
+
+        m_comWeightMatrix.resize(3, 3);
+        m_comWeightMatrix.setFromConstTriplets(comWeightMatrix);
+    }
+
+    tempValue = config.find("neckWeightTriplets");
+    iDynTree::Triplets neckWeightMatrix;
+    if(!iDynTreeHelper::Triplets::getTripletsFromValues(tempValue, 3, neckWeightMatrix))
+    {
+        yError() << "Initialization failed while reading neckWeightTriplets vector.";
+        return false;
+    }
+    m_neckWeightMatrix.resize(3, 3);
+    m_neckWeightMatrix.setFromConstTriplets(neckWeightMatrix);
+
+    // set the matrix related to the joint regularization
+    tempValue = config.find("jointRegularizationWeights");
+    iDynTree::VectorDynSize jointRegularizationWeights(m_actuatedDOFs);
+    if(!YarpHelper::yarpListToiDynTreeVectorDynSize(tempValue, jointRegularizationWeights))
+    {
+        yError() << "Initialization failed while reading jointRegularizationWeights vector.";
+        return false;
+    }
+
+    //  m_jointRegulatizationHessian = H' \lamda H
+    m_jointRegulatizationHessian.resize(m_numberOfVariables, m_numberOfVariables);
+    for(int i = 0; i < m_actuatedDOFs; i++)
+        m_jointRegulatizationHessian(i + 6, i + 6) = jointRegularizationWeights(i);
+
+    // evaluate constant sub-matrix of the gradient matrix
+    m_jointRegulatizationGradient.resize(m_numberOfVariables, m_actuatedDOFs);
+    for(int i = 0; i < m_actuatedDOFs; i++)
+        m_jointRegulatizationGradient(i + 6, i) = jointRegularizationWeights(i);
+
+    // resize matrices
+    m_comJacobian.resize(3, m_numberOfVariables);
+    m_neckJacobian.resize(3, m_numberOfVariables);
+    m_leftFootJacobian.resize(6, m_numberOfVariables);
+    m_rightFootJacobian.resize(6, m_numberOfVariables);
+
+    tempValue = config.find("jointRegularizationGains");
+    iDynTree::VectorDynSize jointRegularizationGains(m_actuatedDOFs);
+    if(!YarpHelper::yarpListToiDynTreeVectorDynSize(tempValue, jointRegularizationGains))
+    {
+        yError() << "Initialization failed while reading jointRegularizationGains vector.";
+        return false;
+    }
+    m_jointRegulatizationGains.resize(m_actuatedDOFs, m_actuatedDOFs);
+    for(int i = 0; i < m_actuatedDOFs; i++)
+        m_jointRegulatizationGains(i, i) = jointRegularizationGains(i);
+
+    if(!YarpHelper::getNumberFromSearchable(config, "k_posFoot", m_kPosFoot))
+    {
+        yError() << "Initialization failed while reading k_posFoot.";
+        return false;
+    }
+
+    if(!YarpHelper::getNumberFromSearchable(config, "k_attFoot", m_kAttFoot))
+    {
+        yError() << "Initialization failed while reading k_attFoot.";
+        return false;
+    }
+
+    if(!YarpHelper::getNumberFromSearchable(config, "k_neck", m_kNeck))
+    {
+        yError() << "Initialization failed while reading k_neck.";
+        return false;
+    }
+
+    if(!YarpHelper::getNumberFromSearchable(config, "k_posCom", m_kCom))
+    {
+        yError() << "Initialization failed while reading k_posCom.";
+        return false;
+    }
+
+    return true;
+}
+
+WalkingQPIK::~WalkingQPIK()
+{ }
+
+bool WalkingQPIK::setRobotState(const iDynTree::VectorDynSize& jointPosition,
+                                     const iDynTree::Transform& leftFootToWorldTransform,
+                                     const iDynTree::Transform& rightFootToWorldTransform,
+                                     const iDynTree::Rotation& neckOrientation,
+                                     const iDynTree::Position& comPosition)
+{
+    if(jointPosition.size() != m_actuatedDOFs)
+    {
+        yError() << "[setRobotState] The size of the jointPosition vector is not coherent with the "
+                 << "number of the actuated Joint";
+        return false;
+    }
+
+    m_jointPosition = jointPosition;
+    m_leftFootToWorldTransform = leftFootToWorldTransform;
+    m_rightFootToWorldTransform = rightFootToWorldTransform;
+    m_neckOrientation = neckOrientation;
+    m_comPosition = comPosition;
+
+    return true;
+}
+
+void WalkingQPIK::setDesiredNeckOrientation(const iDynTree::Rotation& desiredNeckOrientation)
+{
+    m_desiredNeckOrientation =  desiredNeckOrientation * m_additionalRotation;
+}
+
+bool WalkingQPIK::setCoMJacobian(const iDynTree::MatrixDynSize& comJacobian)
+{
+    if(comJacobian.rows() != 3)
+    {
+        yError() << "[setCoMJacobian] the number of rows has to be equal to 3.";
+        return false;
+    }
+    if(comJacobian.cols() != m_actuatedDOFs + 6)
+    {
+        yError() << "[setCoMJacobian] the number of rows has to be equal to" << m_actuatedDOFs + 6;
+        return false;
+    }
+    m_comJacobian = comJacobian;
+
+    return true;
+}
+
+bool WalkingQPIK::setLeftFootJacobian(const iDynTree::MatrixDynSize& leftFootJacobian)
+{
+    if(leftFootJacobian.rows() != 6)
+    {
+        yError() << "[setCoMJacobian] the number of rows has to be equal to 6.";
+        return false;
+    }
+    if(leftFootJacobian.cols() != m_actuatedDOFs + 6)
+    {
+        yError() << "[setCoMJacobian] the number of rows has to be equal to" << m_actuatedDOFs + 6;
+        return false;
+    }
+
+    m_leftFootJacobian = leftFootJacobian;
+
+    return true;
+}
+
+bool WalkingQPIK::setRightFootJacobian(const iDynTree::MatrixDynSize& rightFootJacobian)
+{
+    if(rightFootJacobian.rows() != 6)
+    {
+        yError() << "[setCoMJacobian] the number of rows has to be equal to 6.";
+        return false;
+    }
+    if(rightFootJacobian.cols() != m_actuatedDOFs + 6)
+    {
+        yError() << "[setCoMJacobian] the number of rows has to be equal to" << m_actuatedDOFs + 6;
+        return false;
+    }
+
+    m_rightFootJacobian = rightFootJacobian;
+
+    return true;
+}
+
+bool WalkingQPIK::setNeckJacobian(const iDynTree::MatrixDynSize& neckJacobian)
+{
+    if(neckJacobian.rows() != 6)
+    {
+        yError() << "[setNeckMJacobian] the number of rows has to be equal to 6.";
+        return false;
+    }
+    if(neckJacobian.cols() != m_actuatedDOFs + 6)
+    {
+        yError() << "[setNeckMJacobian] the number of rows has to be equal to" << m_actuatedDOFs + 6;
+        return false;
+    }
+
+    iDynTree::toEigen(m_neckJacobian) = iDynTree::toEigen(neckJacobian).block(3, 0, 3,
+                                                                              m_actuatedDOFs + 6);
+
+    return true;
+}
+
+bool WalkingQPIK::setDesiredJointPosition(const iDynTree::VectorDynSize& regularizationTerm)
+{
+    if(regularizationTerm.size() != m_actuatedDOFs)
+    {
+        yError() << "[setDesiredJointPosition] The number of the desired joint position has to be "
+                 << "equal to the number of actuated joints";
+        return false;
+    }
+
+    m_regularizationTerm = regularizationTerm;
+
+    return true;
+}
+
+void WalkingQPIK::setDesiredFeetTransformation(const iDynTree::Transform& desiredLeftFootToWorldTransform,
+                                                    const iDynTree::Transform& desiredRightFootToWorldTransform)
+{
+    m_desiredLeftFootToWorldTransform = desiredLeftFootToWorldTransform;
+    m_desiredRightFootToWorldTransform = desiredRightFootToWorldTransform;
+}
+
+void WalkingQPIK::setDesiredFeetTwist(const iDynTree::Twist& leftFootTwist,
+                                           const iDynTree::Twist& rightFootTwist)
+{
+    m_leftFootTwist = leftFootTwist;
+    m_rightFootTwist = rightFootTwist;
+}
+
+void WalkingQPIK::setDesiredCoMVelocity(const iDynTree::Vector3& comVelocity)
+{
+    m_comVelocity = comVelocity;
+}
+
+void WalkingQPIK::setDesiredCoMPosition(const iDynTree::Position& desiredComPosition)
+{
+    m_desiredComPosition = desiredComPosition;
+}
+
