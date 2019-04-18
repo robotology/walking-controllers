4eb7bd7e2e7c176c6b754b63b8c3cb66029a4eab
Giulio Romualdi <giulio.romualdi@gmail.com>
2019-04-18 12:35:27 +0200

    add mimimum jerk trajectory in the RetargetingClient

--- modules/Walking_module/src/RetargetingClient.cpp
+++ modules/Walking_module/src/RetargetingClient.cpp
@@ -8,8 +8,11 @@
 
 #include <yarp/os/LogStream.h>
 
-#include <RetargetingClient.hpp>
+#include <iDynTree/yarp/YARPEigenConversions.h>
+#include <iDynTree/Core/EigenHelpers.h>
+
 #include <Utils.hpp>
+#include <RetargetingClient.hpp>
 
 void RetargetingClient::convertYarpVectorPoseIntoTransform(const yarp::sig::Vector& vector,
                                                            iDynTree::Transform& transform)
@@ -19,7 +22,8 @@ void RetargetingClient::convertYarpVectorPoseIntoTransform(const yarp::sig::Vect
 }
 
 bool RetargetingClient::initialize(const yarp::os::Searchable &config,
-                                   const std::string &name)
+                                   const std::string &name,
+                                   const double &period)
 {
     if(config.isNull())
     {
@@ -50,15 +54,45 @@ bool RetargetingClient::initialize(const yarp::os::Searchable &config,
     }
     m_rightHandTransformPort.open("/" + name + portName);
 
+    m_leftHandTransformYarp.resize(6);
+    m_rightHandTransformYarp.resize(6);
+
+    double smoothingTime;
+    if(!YarpHelper::getNumberFromSearchable(config, "smoothing_time", smoothingTime))
+    {
+        yError() << "[RetargetingClient::initialize] Unable to get the number from searchable.";
+        return false;
+    }
+
+    m_leftHandSmoother = std::make_unique<iCub::ctrl::minJerkTrajGen>(6, period, smoothingTime);
+    m_rightHandSmoother = std::make_unique<iCub::ctrl::minJerkTrajGen>(6, period, smoothingTime);
+
     return true;
 }
-
 
 void RetargetingClient::reset(const iDynTree::Transform& leftHandTransform,
                               const iDynTree::Transform& rightHandTransform)
 {
+    if(!m_useHandRetargeting)
+        return;
+
     m_leftHandTransform = leftHandTransform;
     m_rightHandTransform = rightHandTransform;
+
+    iDynTree::toEigen(m_leftHandTransformYarp).block(0, 0, 3, 1) =
+        iDynTree::toEigen(m_leftHandTransform.getPosition());
+
+    iDynTree::toEigen(m_leftHandTransformYarp).block(0, 3, 3, 1) =
+        iDynTree::toEigen(m_leftHandTransform.getRotation().asRPY());
+
+    iDynTree::toEigen(m_rightHandTransformYarp).block(0, 0, 3, 1) =
+        iDynTree::toEigen(m_rightHandTransform.getPosition());
+
+    iDynTree::toEigen(m_rightHandTransformYarp).block(0, 3, 3, 1) =
+        iDynTree::toEigen(m_rightHandTransform.getRotation().asRPY());
+
+    m_leftHandSmoother->init(m_leftHandTransformYarp);
+    m_rightHandSmoother->init(m_rightHandTransformYarp);
 }
 
 void RetargetingClient::getFeedback()
@@ -68,11 +102,17 @@ void RetargetingClient::getFeedback()
 
     auto desiredLeftHandPose = m_leftHandTransformPort.read(false);
     if(desiredLeftHandPose != nullptr)
-        convertYarpVectorPoseIntoTransform(*desiredLeftHandPose, m_leftHandTransform);
+        m_leftHandTransformYarp = *desiredLeftHandPose;
+
+    m_leftHandSmoother->computeNextValues(m_leftHandTransformYarp);
+    convertYarpVectorPoseIntoTransform(m_leftHandSmoother->getPos(), m_leftHandTransform);
 
     auto desiredRightHandPose = m_rightHandTransformPort.read(false);
     if(desiredRightHandPose != nullptr)
-        convertYarpVectorPoseIntoTransform(*desiredRightHandPose, m_rightHandTransform);
+        m_rightHandTransformYarp = *desiredRightHandPose;
+
+    m_rightHandSmoother->computeNextValues(m_rightHandTransformYarp);
+    convertYarpVectorPoseIntoTransform(m_rightHandSmoother->getPos(), m_rightHandTransform);
 }
 
 const iDynTree::Transform& RetargetingClient::leftHandTransform() const
