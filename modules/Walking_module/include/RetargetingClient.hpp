2ff1ebcbe087f781ba8d425e3ced1861cb517398
Giulio Romualdi <giulio.romualdi@gmail.com>
2019-04-18 12:35:27 +0200

    add mimimum jerk trajectory in the RetargetingClient

--- modules/Walking_module/include/RetargetingClient.hpp
+++ modules/Walking_module/include/RetargetingClient.hpp
@@ -9,10 +9,14 @@
 #ifndef RETARGETING_CLIENT_HPP
 #define RETARGETING_CLIENT_HPP
 
+#include <memory>
+
 #include <iDynTree/Core/Transform.h>
 
 #include <yarp/os/BufferedPort.h>
 #include <yarp/sig/Vector.h>
+
+#include <iCub/ctrl/minJerkCtrl.h>
 
 class RetargetingClient
 {
@@ -21,10 +25,20 @@ private:
     iDynTree::Transform m_leftHandTransform;
     iDynTree::Transform m_rightHandTransform;
 
+    yarp::sig::Vector m_leftHandTransformYarp;
+    yarp::sig::Vector m_rightHandTransformYarp;
+
     yarp::os::BufferedPort<yarp::sig::Vector> m_leftHandTransformPort;
     yarp::os::BufferedPort<yarp::sig::Vector> m_rightHandTransformPort;
 
     bool m_useHandRetargeting;
+
+    std::unique_ptr<iCub::ctrl::minJerkTrajGen> m_leftHandSmoother; /**< Minimum jerk trajectory
+                                                                       for the left hand. */
+
+    std::unique_ptr<iCub::ctrl::minJerkTrajGen> m_rightHandSmoother; /**< Minimum jerk trajectory
+                                                                        for the right hand. */
+
 
     void convertYarpVectorPoseIntoTransform(const yarp::sig::Vector& vector,
                                             iDynTree::Transform& transform);
@@ -32,7 +46,8 @@ private:
 public:
 
     bool initialize(const yarp::os::Searchable &config,
-                    const std::string &name);
+                    const std::string &name,
+                    const double &period);
 
     void reset(const iDynTree::Transform& leftHandTransform,
                const iDynTree::Transform& rightHandTransform);
