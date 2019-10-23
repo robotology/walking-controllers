5c2f974ef007cd7f184fb62d6fd71c3a1f9d4075
Milad <milad.shafiee@iit.it>
2019-10-23 10:55:40 +0200

    removing autogenerated files

--- modules/StepAdaptation/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/milad/software/robotology-superbuild/robotology/walking-controllers/modules/StepAdaptation/test
-# Build directory: /home/milad/software/robotology-superbuild/robotology/walking-controllers/modules/StepAdaptation/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(StepAdaptationTest "/home/milad/software/robotology-superbuild/robotology/walking-controllers/modules/StepAdaptation/test/StepAdaptationTest")
