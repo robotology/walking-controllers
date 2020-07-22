8951ccc126a6d737709656b464ca25f6c9f3093c
Stefano <stefano.dafarra@gmail.com>
2020-07-22 20:57:34 +0200

    Yet another test, checking the stance phase in another way.

--- src/WalkingModule/src/Module.cpp
+++ src/WalkingModule/src/Module.cpp
@@ -852,7 +852,9 @@ bool WalkingModule::updateModule()
         // inner COM-ZMP controller
         // if the the norm of desired DCM velocity is lower than a threshold then the robot
         // is stopped
-        m_walkingZMPController->setPhase(m_isStancePhase.front());
+        double threshold = 0.001;
+        bool stancePhase = iDynTree::toEigen(m_DCMVelocityDesired.front()).norm() < threshold;
+        m_walkingZMPController->setPhase(stancePhase);
 
         iDynTree::Vector2 desiredZMP;
         if(m_useMPC)
