092b230be62592447040149812810017d894976f
Stefano Dafarra <stefano.dafarra@gmail.com>
2022-09-28 13:32:39 +0200

    Update WalkingControllersDependencies.cmake

--- cmake/WalkingControllersDependencies.cmake
+++ cmake/WalkingControllersDependencies.cmake
@@ -7,7 +7,7 @@ find_package(ICUB REQUIRED)
 find_package(ICUBcontrib REQUIRED)
 find_package(iDynTree REQUIRED)
 find_package(Eigen3 3.2.92 REQUIRED)
-find_package(UnicyclePlanner 0.5.1 REQUIRED)
+find_package(UnicyclePlanner 0.5.2 REQUIRED)
 find_package(OsqpEigen 0.4.0 REQUIRED)
 find_package(qpOASES REQUIRED)
 find_package(BipedalLocomotionFramework 0.9.0
