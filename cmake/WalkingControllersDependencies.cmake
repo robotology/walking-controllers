dfbb4d0611e6363b11915695773c14febfcaf37a
Stefano <stefano.dafarra@gmail.com>
2024-02-28 17:49:56 +0100

    Set the last step DCM stillness from configuration file

--- cmake/WalkingControllersDependencies.cmake
+++ cmake/WalkingControllersDependencies.cmake
@@ -7,7 +7,7 @@ find_package(ICUB REQUIRED)
 find_package(ICUBcontrib REQUIRED)
 find_package(iDynTree REQUIRED)
 find_package(Eigen3 3.2.92 REQUIRED)
-find_package(UnicyclePlanner 0.6.0 REQUIRED)
+find_package(UnicyclePlanner 0.8.0 REQUIRED)
 find_package(OsqpEigen 0.4.0 REQUIRED)
 find_package(BipedalLocomotionFramework 0.18.0
   COMPONENTS VectorsCollection IK ParametersHandlerYarpImplementation
