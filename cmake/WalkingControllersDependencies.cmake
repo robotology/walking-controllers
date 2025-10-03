62bd121f5874733a076df080acd7a3d7f554026d
Silvio Traversaro <silvio.traversaro@iit.it>
2025-10-03 11:36:30 +0200

    Remove version constraint for Eigen3 package for Eigen3 5.0.0 compat

--- cmake/WalkingControllersDependencies.cmake
+++ cmake/WalkingControllersDependencies.cmake
@@ -6,7 +6,7 @@ find_package(YARP 3.6.0 REQUIRED)
 find_package(ICUB REQUIRED)
 find_package(ICUBcontrib REQUIRED)
 find_package(iDynTree 10.0.0 REQUIRED)
-find_package(Eigen3 3.2.92 REQUIRED)
+find_package(Eigen3 REQUIRED)
 find_package(UnicyclePlanner 0.8.0 REQUIRED)
 find_package(OsqpEigen 0.4.0 REQUIRED)
 find_package(BipedalLocomotionFramework 0.18.0
